add89bc38703128241f6009e5a53c6fa1495e3f7
syrgo <syrga.v30@gmail.com>
2022-03-02 15:26:15 +0300

    modals has been created

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.accordion').forEach(accordion => {
+  const parent = accordion.closest('.accordions');
+
+  accordion.querySelector('.accordion__header').addEventListener('click', function() {
+    if(parent && parent.classList.contains('accordions--toggle')) {
+      parent.querySelectorAll('.accordion').forEach(accordion => {
+        accordion.classList.remove("accordion--open");
+      });
+    }
+
+    accordion.classList.add('accordion--open');
+  })
+})
